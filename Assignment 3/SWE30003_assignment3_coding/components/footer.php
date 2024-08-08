27f83114a922d7516e30b9c3eb40b6768e8a5988
dungbi3011 <quocdungtran2003@gmail.com>
2024-08-09 03:51:26 +0700

    Kiến trúc Dữ liệu

---
+++ Assignment 3/SWE30003_assignment3_coding/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">swinburne@gmail.com</a>
+         <a href="mailto:anasbhai@gmail.com">quocdungtran2003@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00AM to 11:00PM</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Australia, Hawthorn, ....</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">0943910306</a> <!-- Developer phone number -->
+         <a href="tel:1112223333">04-21943404</a> <!-- Australia number -->
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Swinburne Student</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
